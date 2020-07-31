e6242e51bc5152c06fed94ef8228131adf536217
Grant Smith <grantnsmith@gmail.com>
2020-07-30 18:17:58 -0700

    updated apiRoutes with react index.html route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const router = require("express").Router();
 const axios = require("axios");
 require("dotenv").config();
+
+// React app
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 router.route("/legislators/:state", (req, res) => {
   axios
