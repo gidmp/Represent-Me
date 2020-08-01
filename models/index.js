58286d5eb8034b3799517fd82e63359fa635c8ca
bmaha2 <maharjanbipin@gmail.com>
2020-08-01 12:25:51 -0700

    adds routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,57 @@
-module.exports = {
-    Db: require("./Db")
-};
-  
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const emailValidator = require("email-validator");
+const SALT_ROUNDS = 12;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        minlength: 3,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        minlength: 6,
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: emailValidator.validate,
+            message: props => `${props.value} is not a valid email address`
+        }
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+
+});
+UserSchema.methods.validPassword = function validatePassword(candidate) {
+    return bcrypt.compare(candidate, this.password);
+}
+
+UserSchema.pre('save', function presSave(next) {
+    const user = this;
+    const hash = bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    next();
+})
+module.exports = mongoose.model("User", UserSchema);
+
+
--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose");
-const routes = require("./routes");
+//const routes = require("./routes/api-routes");
 const app = express();
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
+
+
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(
+  session({ secret: "super secret 12345", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Add routes, both API and view
-app.use(routes);
+//app.use(routes);
+
+require("./routes/api-routes")(app);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/UserDb", () => {
+  console.log(`Succcessfully Connected to Db`);
+});
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
 
 // Start the API server
 app.listen(PORT, function() {
