15aa586b0f199884eba5e8cfe7518e660bf99fb1
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-08-05 18:33:22 -0700

    added props.children to section

--- client/src/components/Section/index.js
+++ client/src/components/Section/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Section(props) {
     return (
-        <div class="uk-section">
-            <div class="uk-container">
-                <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+        <div className="uk-section">
+            <div className="uk-container">
+                <div className="uk-grid-match uk-child-width-1-3@m" uk-grid = "true">
                     {props.children}
                 </div>
             </div>
