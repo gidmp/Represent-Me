36608015460fdc033d3a880839d1d428b7ba38ee
julieritz <julie.ritz@gmail.com>
2020-08-06 21:37:36 -0700

    new navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -12,18 +12,19 @@ function Nav(props){
                 <nav className="uk-navbar-container" uk-navbar = "true">
                     <div className="uk-navbar-left">
                         <ul className="uk-navbar-nav uk-visible@m">
-                            <li><Link to = "/">Home</Link></li>
-                            <li><Link to = "/landingpage">Profile</Link></li>
+                            <li><Link to = "/landingpage">My Representatives</Link></li>
+                            <li><Link to = "/education">Learn</Link></li>
                         </ul>
                     </div>
                     <div className="uk-navbar-center">
-                        <img id = "mugshot" src={Logo} alt="Me" style={{ width: 300 }} />
+                        <Link to = "/"><img id = "mugshot" src={Logo} alt="Represent Me" style={{ width: 300 }} /></Link>
                         {/* <a className="uk-navbar-item uk-logo" href="#">{props.pageTitle}</a> */}
                     </div>
                     <div className="uk-navbar-right">
                         <ul className="uk-navbar-nav uk-visible@m">
-                            <li><Link to = "/education">Learn</Link></li>
                             <li><Link to = "/about">About</Link></li>
+                            <li><Link to = "/login">Login</Link></li>
+                            <li><Link to = "/">Logout</Link></li>
                         </ul>
                         <a className="hamburger-menu uk-hidden@m" href="#mobileNav" uk-toggle="target: #mobileNav"><span className="menu-icon"uk-icon="icon: menu; ratio: 1.8"></span></a>
                     </div>
