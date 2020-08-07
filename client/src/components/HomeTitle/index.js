b176d16b37a0a8b33d68e7e42cb5e22ffd4cfd0b
julieritz <julie.ritz@gmail.com>
2020-08-06 18:53:54 -0700

    homtitle fix

--- client/src/components/HomeTitle/index.js
+++ client/src/components/HomeTitle/index.js
@@ -6,12 +6,11 @@ function HomeTitle(){
     const location = useLocation();
     return(
         <div className="uk-text-center home-title">
-
-            <p uk-margin = "true">
-                <button className="uk-button uk-button-primary uk-button-large home-btn"><Link  className={location.pathname === "/login"}to = "/login">Login</Link></button>
-                <button className="uk-button uk-button-primary uk-button-large home-btn"><Link className={location.pathname === "/signUp"} to = "/signup">Sign Up</Link></button>
-            </p>        
-        </div>
+        <p uk-margin = "true">
+        <Link  className={location.pathname === "/login"}to = "/login"><button className="uk-button uk-button-large home-btn">Login</button></Link>
+        <Link className={location.pathname === "/signUp"} to = "/signup"><button className="uk-button uk-button-large home-btn">Sign Up</button></Link>
+        </p>        
+    </div>
     )
 }
 export default HomeTitle;
